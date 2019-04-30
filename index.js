64042395cfd2519b36f869d39d3a2998b10a5caa
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 18:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyRemoveLastKitten() {
     return kittens;
 }
 
-function destructivelyRemoveLastKitten() {
+function destructivelyRemoveFirstKitten() {
     kittens.pop();
     return kittens;
 }
